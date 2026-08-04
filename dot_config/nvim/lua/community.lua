7ac63c94fcb8c8ee653906a7014afff44672deab
Aly Raffauf <aly@aly.codes>
2026-08-04 13:09:59 -0400

    Add .chezmoidata/nix-packages.json Update .Brewfile Update .config/nvim/lazy-lock.json Update .config/nvim/lua/community.lua Add install-nix-profile-packages.sh

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -7,7 +7,18 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.chezmoi" },
+  { import = "astrocommunity.pack.gleam" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
   { import = "astrocommunity.recipes.neovide" },
   { import = "astrocommunity.colorscheme.catppuccin" },
